c6d79af41e4a6a56d82d8f8ce4672dacf4edc3a6
Alumno <alumno@ejemplo.com>
2026-06-11 17:52:00 +0200

    Modificar MiClase en rama segunda

--- src/MiClase.java
+++ src/MiClase.java
@@ -1,2 +1,2 @@
-public class MiClase {    public static void main(String[] args) {
-    System.out.println("Hola desde Git");    }}
+public class MiClase {    public static void main(String[] args) {        System.out.println("Hola desde Git!");        System.out.println("Modificación en rama segunda");    }       public void nuevoMetodo() {
+    System.out.println("Método añadido en rama segunda");    }}
